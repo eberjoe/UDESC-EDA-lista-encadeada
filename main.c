859d4ad9d93332c1c145b47513411084a00758a7
Eber Martins Rodrigues <eber.rodrigues@neogrid.com>
2019-10-23 17:59:59 -0300

    Update tests

--- main.c
+++ main.c
@@ -1,79 +1,86 @@
 #include "lista.h"
 
-void mostra_float( void *x);
+void mostra_float(void *x);
 int compara_float(void *x, void *y);
 
 int main () {
     Lista l1;
     float x = 123.91239, y;
+    char r[2][11] = {"falhou!", "deu certo!"};
 
     inicializa_lista(&l1, sizeof(float));
 
-    insereNoInicio(&l1, &x); // testa inserção no início
+    printf("\nA insercao no inicio de %f %s\n", x, r[insereNoInicio(&l1, &x)]);
+
+    //mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x = 3.12;
 
-    insereNoInicio(&l1, &x); // testa inserção no início
+    printf("\nA insercao no inicio de %f %s\n", x, r[insereNoInicio(&l1, &x)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x=0.6;
 
-    insereNoInicio(&l1, &x); // testa inserção no início
+    printf("\nA insercao no inicio de %f %s\n", x, r[insereNoInicio(&l1, &x)]);
 
     mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x = 5;
 
-    insereNoFim(&l1, &x); // testa inserção no final
+    printf("\nA insercao no fim de %f %s\n", x, r[insereNoFim(&l1, &x)]);
 
     mostra_lista(l1, mostra_float); // mostra toda a lista
 
-    removeDoInicio(&l1, &y); // testa remoção do início
+    printf("\nA remocao do inicio de %f %s\n", y, r[removeDoInicio(&l1, &y)]);
 
-    removeDoFim(&l1, &y); // testa remoção do fim
+    mostra_lista(l1, mostra_float); // mostra toda a lista
+
+    printf("\nA remocao do fim de %f %s\n", y, r[removeDoFim(&l1, &y)]);
 
     mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x = 9;
 
-    insereNoFim(&l1, &x);
+    printf("\nA insercao no fim de %f %s\n", x, r[insereNoFim(&l1, &x)]);
 
     mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x = 12;
 
-    insereNaPos(&l1, &x, 2);
+    printf("\nA insercao na posicao 2 de %f %s\n", x, r[insereNaPos(&l1, &x, 2)]);
 
     mostra_lista(l1, mostra_float); // mostra toda a lista
 
-    removeDaPos(&l1, &y, 1);
+    printf("\nA remocao da posicao 1 de %f %s\n", y, r[removeDaPos(&l1, &y, 1)]);
 
     mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x = 10000000;
 
-    insereNaOrdem(&l1, &x, compara_float);
+    printf("\nA insercao ordenada de %f %s\n", x, r[insereNaOrdem(&l1, &x, compara_float)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x = 1.2;
 
-    insereNaOrdem(&l1, &x, compara_float);
+    printf("\nA insercao ordenada de %f %s\n", x, r[insereNaOrdem(&l1, &x, compara_float)]);
+
+    mostra_lista(l1, mostra_float); // mostra toda a lista
 
     x = 13;
 
-    insereNaOrdem(&l1, &x, compara_float);
+    printf("\nA insercao ordenada de %f %s\n", x, r[insereNaOrdem(&l1, &x, compara_float)]);
 
     mostra_lista(l1, mostra_float);
 
     x = 144000;
 
-    printf("Modificando a posicao 1");
-
-    modificaNaPos(&l1, &x, 1));
+    printf("\nA sobrescrita de %f na posicao 1 %s\n", x, r[modificaNaPos(&l1, &x, 1)]);
 
     mostra_lista(l1, mostra_float);
 
-    leNaPos(&l1, &y, 2);
-
-    printf("A posicao 2 tem %f", y);
+    printf("\nA leitura de %f na posicao 2 %s\n", y, r[leNaPos(&l1, &y, 2)]);
 
     limpa_lista(&l1); // testa limpeza
 
